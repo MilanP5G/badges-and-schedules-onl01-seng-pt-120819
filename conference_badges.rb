0b30fc0916d076682cabb09de280af7ed4cb9aa5
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-13 11:25:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,4 +9,10 @@ def batch_badge_maker(attendees)
   attendees.collect {|attendee| badge_maker(attendee)}
 
 end
+
+def assign_rooms(names, index)
+  
+  names.map.with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  
+end
  
